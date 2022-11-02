caf8148ef74bf047dc44fbdf9d34441f7035f344
Eko Yanuarso Budi <36530602+ekoyanu99@users.noreply.github.com>
2022-11-02 21:59:24 +0700

    final contract v1

---
+++ truffle-config.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+//Account credentials from which our contract will be deployed
+const mnemonic = process.env.MNEMONIC;
+
+//API key of your Datahub account for Avalanche Fuji test network
+const APIKEY = process.env.APIKEY;
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 9545,
+      network_id: "*"
+    },
+    polygon: {
+      provider: function() {
+        return new HDWalletProvider({mnemonic, providerOrUrl: `https://rpc.ankr.com/polygon_mumbai`})
+      },
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+      gas: 6000000,
+      gasPrice: 10000000000,
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
+  plugins: [
+    'truffle-plugin-verify'
+  ], 
+  api_keys: {
+    polygonscan: process.env.POLYGONSCANAPI
+  }
+}
