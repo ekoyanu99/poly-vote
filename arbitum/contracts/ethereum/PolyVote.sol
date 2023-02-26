226f9f50e73311783c5d89afc49a4fe5b1fb13b7
Eko Yanuarso Budi <36530602+ekoyanu99@users.noreply.github.com>
2023-02-26 11:34:32 +0700

    add goerli and plugin verify

---
+++ arbitum/contracts/ethereum/PolyVote.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+import "./Election.sol";
+
+contract PolyVote is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+
+    mapping(address => bool) internal isWhiteListed;
+
+    Election private _election;
+
+    constructor() ERC721("PolyVote", "PVT") {
+        _tokenIdCounter.increment();
+    }
+
+    // add remove whitelist
+    function addWhiteList(address _newUser) external onlyOwner {
+        isWhiteListed[_newUser] = true;
+    }
+
+    function removeWhiteList(address _newUser) external onlyOwner {
+        isWhiteListed[_newUser] = false;
+    }
+
+    //check status
+    function check() external view returns (string memory) {
+        if (isWhiteListed[msg.sender] == true) {
+            return "you are on whitelist";
+        } else {
+            return "you are not whitelisted";
+        }
+    }
+
+    Counters.Counter private _tokenIdCounter;
+    uint256 maxSupply = 10000;
+
+    function safeMint(address to, string memory uri) public {
+        // require(_election.getStart() == true);
+        require(
+            isWhiteListed[msg.sender] == true,
+            "User not authorized to mint"
+        );
+        require(
+            _tokenIdCounter.current() <= maxSupply,
+            "I'm sorry we reached the cap"
+        );
+        require(balanceOf(msg.sender) == 0, "Max mint per wallet reached");
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        uint256 batchSize
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    function _burn(
+        uint256 tokenId
+    ) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
--- arbitum/truffle-config.js
+++ arbitum/truffle-config.js
@@ -3,6 +3,7 @@
 require('dotenv').config();
 const mnemonic = process.env["MNEMONIC"];
 const alchemyKey = process.env["ALCHEMY_KEY"];
+const goerliKey = process.env["GOERLI_KEY"]
 
 //uncomment to use mainnetMnemonic, be sure to set it in the .env file
 //const mainnetMnemonic = process.env["MAINNET_MNEMONIC"]
@@ -14,34 +15,19 @@ module.exports = {
   /**
   * contracts_build_directory tells Truffle where to store compiled contracts
   */
-  contracts_build_directory: './build/arbitrum-contracts',
+  // contracts_build_directory: './build/arbitrum-contracts',
+  contracts_build_directory: './build/ethereum-contracts',
 
   /**
   *  contracts_directory tells Truffle where to find your contracts
   */
-  contracts_directory: './contracts/arbitrum',
+  // contracts_directory: './contracts/arbitrum',
+  contracts_directory: './contracts/ethereum',
 
   networks: {
     development: {
       url: "http://127.0.0.1:9545",
       network_id: "*",
-    },
-    // for use with local environment -- use `npm runLocalArbitrum` to start
-    // after you have installed the repo and run `npm runLocalEthereum`, which will run a test L1 chain
-    // **please note, this network does not currently work; we will remove this line once the issue is fixed!
-    arbitrum_local: {
-      network_id: "*",
-      gas: 8500000,
-      provider: function() {
-        return new HDWalletProvider({
-          mnemonic: {
-            phrase: mnemonic
-          },
-          providerOrUrl: "http://127.0.0.1:8547/",
-          addressIndex: 0,
-          numberOfAddresses: 1
-        })
-      }
     },
     arbitrum_testnet: {
       network_id: 421613,
@@ -67,6 +53,11 @@ module.exports = {
         return new HDWalletProvider(mnemonic, "https://arbitrum-mainnet.infura.io/v3/" + alchemyKey, 0, 1);
       }
     },
+    goerli: {
+      provider: () => new HDWalletProvider(mnemonic, "https://eth-goerli.g.alchemy.com/v2/MRRdG0d-MJXVwl5ogBT_pkUHlbWcqxkl"),
+      network_id: 5,
+      gas: 4465030
+    }
   },
 
   mocha: {
@@ -85,5 +76,11 @@ module.exports = {
   },
   db: {
     enabled: false
+  },
+  plugins: [
+    'truffle-plugin-verify'
+  ], 
+  api_keys: {
+    etherscan: process.env.ETHERSCANAPI
   }
 }
